23dfc4770fed7ce2d53407a7d976ed5adf2092d7
rdebowski <rdebowski@coders.center>
2017-03-04 13:22:10 +0100

    Report Settings Service

---
+++ code/Common/Constants.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserAuditReport.Common
+{
+    public static class Constants
+    {
+        public static class ReportsSettings
+        {
+            public static ID ReportsSettingsItemId
+            {
+                get
+                {
+                    return new ID("{2F8BAFC0-7253-4BB2-916D-0000FAB46185}");
+                }
+            }
+        }
+    }
+}
---
+++ code/Services/ReportSettingsService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UserAuditReport.DTO;
+
+namespace UserAuditReport.Services
+{
+    public class ReportSettingsService : IReportSettingsService
+    {
+        public ReportSettingsDTO GetReportSettings()
+        {
+            var settingsItem = Sitecore.Context.Database.GetItem(Common.Constants.ReportsSettings.ReportsSettingsItemId);
+
+            Sitecore.Data.Fields.MultilistField trackedRoles = settingsItem.Fields["Tracked Roles"];
+            var trackedRolesList = trackedRoles.Items.ToList();
+
+            var reportSetttingDTO = new ReportSettingsDTO()
+            {
+                   TrackedRoles = trackedRolesList
+            };
+
+            return reportSetttingDTO;
+        }
+    }
+}
