f87e8cf7324d92b1a47c5deeb5d92754b46e0fc0
Tomasz Juranek <tjuranek@coders.center>
2017-03-04 16:35:27 +0100

    load data from controller

--- code/Controllers/UserAuditReportController.cs
+++ code/Controllers/UserAuditReportController.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using UserAuditReport.DTO;
 
 namespace UserAuditReport.Controllers
 {
-    public class UserAuditReportController
+    public class UserAuditReportController : Controller
     {
+        public ActionResult GetUsers()
+        {
+            List<UserChangeDto> list = new List<UserChangeDto>();
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
     }
 }
--- code/sitecore/shell/client/Applications/UserAuditReport/Renderings/UserAuditList.js
+++ code/sitecore/shell/client/Applications/UserAuditReport/Renderings/UserAuditList.js
@@ -1,7 +1,25 @@
 ﻿define(["sitecore"], function (Sitecore) {
   var model = Sitecore.Definitions.Models.ControlModel.extend({
     initialize: function (options) {
-      this._super();
+        this._super();
+        app = this;
+        app.set("userlist", "");
+        app.GetUsers(app);
+    },
+
+    GetUsers: function (app) {
+        jQuery.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "/api/sitecore/UserAuditReport/GetUsers",
+            cache: false,
+            success: function (data) {
+                app.set("userlist", data);
+            },
+            error: function () {
+                console.log("Error in GetUsers() function");
+            }
+        });
     }
   });
 
